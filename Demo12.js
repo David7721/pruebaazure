f2a3373115f9dd09716f371b383c75a946a51be0
David7721 <105019824+David7721@users.noreply.github.com>
2022-05-05 17:45:55 -0500

    Add files via upload

---
+++ Demo12.js
@@ -0,0 +1,6 @@
+function Conver()
+		 {
+            var n1 = document.getElementById('celsius').value;
+            var Convertir = (parseInt(n1)*1.8)+32;
+            alert("Grados F: "+Convertir)
+        }
